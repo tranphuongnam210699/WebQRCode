bc47abcf6b492680383e83012d5ced787665c6e0
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-02-24 23:52:57 +0700

    update date 24/2/2020

--- project-version-01/src/App.js
+++ project-version-01/src/App.js
@@ -3,8 +3,26 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import component
+import Navbar from './components/navbar';
+import EditExercise from './components/editExercise';
+import ExerciseList from './components/exerciseList';
+import CreateExercise from './components/createExercise';
+import CreateUser from './components/createUser';
+
 function App() {
-    return <div className='container'>hello world!!!</div>;
+    return (
+        <Router>
+            <Navbar />
+            <br />
+            <div className='container'>
+                <Route path='/' exact component={ExerciseList} />
+                <Route path='/edit/:id' exact component={EditExercise} />
+                <Route path='/create' exact component={CreateExercise} />
+                <Route path='/user' exact component={CreateUser} />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ project-version-01/src/components/exerciseList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class exerciseList extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the exercise list component</p>
+            </div>
+        );
+    }
+}
