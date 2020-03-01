f99df088c83dc74ef0486b91f7b0dec210238f92
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-03-01 21:24:36 +0700

    commit code scan qr code

--- project-version-01/src/components/Home.js
+++ project-version-01/src/components/Home.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import QrReader from "react-qr-reader";
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            delay: 300,
+            result: "No result"
+        };
+        this.handleScan = this.handleScan.bind(this);
+    }
+    handleScan(data) {
+        if (data) {
+            this.setState({
+                result: data
+            });
+        }
+    }
+    handleError(err) {
+        console.error(err);
+    }
     render() {
         return (
             <div>
-                <span>this is page Home</span>
+
+                {/* <QrReader
+                    delay={this.state.delay}
+                    onError={this.handleError}
+                    onScan={this.handleScan}
+                    style={{ width: "300px", height: "300px" }}
+                />
+                <p>{this.state.result}</p> */}
+
+                this is page Home
             </div>
-        )
+        );
     }
 }
