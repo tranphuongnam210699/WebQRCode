d9b20a72efc584cf043b4d9458c5ba24f1b52747
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-03-13 21:10:50 +0700

    commit upload image

--- project-version-01/src/App.js
+++ project-version-01/src/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import component
-import Navbar from './components/Navbar';
+
 import CreateUser from './components/createUser';
 import ProductList from './Page/Products/ProductList';
 import Home from './components/Home';
 import AddProduct from './Page/Products/AddProduct';
 // import QRCode from './components/QRCode';
 import ScanQRCode from './Page/ScanQRCode/ScanQRCdoe';
+import Navbar from './components/navbar';
 
 function App() {
     return (
--- project-version-01/src/Page/Products/AddProduct.js
+++ project-version-01/src/Page/Products/AddProduct.js
@@ -1,5 +1,26 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
+
+import { Upload, message } from "antd";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+
+function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => callback(reader.result));
+    reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+    if (!isJpgOrPng) {
+        message.error("You can only upload JPG/PNG file!");
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+        message.error("Image must smaller than 2MB!");
+    }
+    return isJpgOrPng && isLt2M;
+}
 
 export default class AddProduct extends Component {
     constructor(props) {
@@ -9,19 +30,21 @@ export default class AddProduct extends Component {
         this.onChangeDescription = this.onChangeDescription.bind(this);
         this.onChangeLoaiID = this.onChangeLoaiID.bind(this);
         this.onChangeNSXID = this.onChangeNSXID.bind(this);
-        this.onChangeName= this.onChangeName.bind(this);
+        this.onChangeName = this.onChangeName.bind(this);
         this.onChangePrice = this.onChangePrice.bind(this);
         this.onChangeQRCode = this.onChangeQRCode.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
+            Image: "",
             Name: "",
             LoaiID: "",
             QRCode: "",
             BarCode: "",
             Description: "",
             NsxID: "",
-            Price: ""
+            Price: "",
+            loading: false
         };
     }
 
@@ -69,9 +92,28 @@ export default class AddProduct extends Component {
         });
     }
 
+    handleChange = info => {
+        if (info.file.status === "uploading") {
+            this.setState({ loading: true });
+            return;
+        }
+        if (info.file.status === "done") {
+            // Get this url from response in real world.
+            getBase64(info.file.originFileObj, imageUrl =>
+                this.setState({
+                    imageUrl,
+                    loading: false,
+                    Image: info.file.originFileObj
+                })
+            );
+        }
+        console.log(info);
+    };
+
     onSubmit(e) {
         e.preventDefault();
         const {
+            Image,
             Name,
             LoaiID,
             QRCode,
@@ -81,6 +123,7 @@ export default class AddProduct extends Component {
             Price
         } = this.state;
         const product = {
+            Image: Image,
             Name: Name,
             LoaiID: LoaiID,
             QRCode: QRCode,
@@ -100,63 +143,98 @@ export default class AddProduct extends Component {
     }
 
     render() {
+        const uploadButton = (
+            <div>
+                {this.state.loading ? <LoadingOutlined /> : <PlusOutlined />}
+                <div className="ant-upload-text">Upload</div>
+            </div>
+        );
+
+        const { imageUrl } = this.state;
+
         return (
-            <div className='pageAddProduct'>
+            <div className="pageAddProduct">
                 <h3>Create New Exercise Log</h3>
                 <form onSubmit={this.onSubmit}>
+                    <Upload
+                        name="avatar"
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        showUploadList={false}
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                        beforeUpload={beforeUpload}
+                        onChange={this.handleChange}
+                    >
+                        {imageUrl ? (
+                            <img
+                                src={imageUrl}
+                                alt="avatar"
+                                style={{ width: "100%" }}
+                            />
+                        ) : (
+                            uploadButton
+                        )}
+                    </Upload>
                     <div className="form-group">
                         <label>Name: </label>
-                        <input type="text"
+                        <input
+                            type="text"
                             required
-                            className='form-control'
+                            className="form-control"
                             onChange={this.onChangeName}
                         />
                     </div>
                     <div className="form-group">
                         <label>LoaiID: </label>
-                        <input type="text"
+                        <input
+                            type="text"
                             required
-                            className='form-control'
+                            className="form-control"
                             onChange={this.onChangeLoaiID}
                         />
                     </div>
                     <div className="form-group">
                         <label>QRCode: </label>
-                        <input type="text"
+                        <input
+                            type="text"
                             required
-                            className='form-control'
+                            className="form-control"
                             onChange={this.onChangeQRCode}
                         />
                     </div>
                     <div className="form-group">
                         <label>BarCode: </label>
-                        <input type="text"
+                        <input
+                            type="text"
                             required
-                            className='form-control'
+                            className="form-control"
                             onChange={this.onChangeBarCode}
                         />
                     </div>
                     <div className="form-group">
                         <label>Description: </label>
-                        <input type="text"
+                        <input
+                            type="text"
                             required
-                            className='form-control'
+                            className="form-control"
                             onChange={this.onChangeDescription}
                         />
                     </div>
                     <div className="form-group">
                         <label>NSXID: </label>
-                        <input type="text"
+                        <input
+                            type="text"
                             required
-                            className='form-control'
+                            className="form-control"
                             onChange={this.onChangeNSXID}
                         />
                     </div>
                     <div className="form-group">
                         <label>Price: </label>
-                        <input type="text"
+                        <input
+                            type="text"
                             required
-                            className='form-control'
+                            className="form-control"
                             onChange={this.onChangePrice}
                         />
                     </div>
